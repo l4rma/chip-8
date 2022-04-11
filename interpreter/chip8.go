ce5df13a3198e6256adbf4f8b4f06ca95dd03853
l4rma <lars.magelssen@gmail.com>
2022-04-11 12:41:41 +0200

    able to load rom

--- interpreter/chip8.go
+++ interpreter/chip8.go
@@ -1,9 +1,16 @@
 package interpreter
 
+import "io"
+
 type chip8 struct {
 	memory [4096]byte
 }
 
 func NewChip8() chip8 {
 	return chip8{}
+}
+
+func (c *chip8) LoadRom(data io.Reader) (int, error) {
+	offset := 0x200
+	return data.Read(c.memory[offset:])
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/l4rma/chip-8/interpreter"
 )
 
 func main() {
 	chip8 := interpreter.NewChip8()
+	game, err := os.Open("./roms/space_invaders.ch8")
+	if err != nil {
+		log.Panicf("Error opening file: %s", err)
+	}
+	chip8.LoadRom(game)
 	fmt.Printf("Chip8: %v", chip8)
 }
